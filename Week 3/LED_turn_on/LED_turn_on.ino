fdbc01afbc6bc28e5a0aa243176112975fa16652
Xinjie <xhuang2@uw.edu>
2021-04-20 02:25:21 -0500

    Update LED_turn_on.ino

--- Week 3/LED_turn_on/LED_turn_on.ino
+++ Week 3/LED_turn_on/LED_turn_on.ino
@@ -5,8 +5,7 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(3, HIGH);  // turn LED on (output 5V)
-  delay(1000);            // wait one second
-  digitalWrite(3, LOW);   // turn LED off (output 0V)
-  delay(1000);            // wait another second
+  // put your setup code here, to run once:
+  pinMode(3, OUTPUT);
+  digitalWrite(3, HIGH); // turn LED on (output 5V)
 }
