755aed3d2864ee30698a2052088e78c3bf96aa9f
Xinjie <xhuang2@uw.edu>
2021-04-20 03:21:07 -0500

    Update rgb_led.ino

--- Week 3/rgb_led/rgb_led.ino
+++ Week 3/rgb_led/rgb_led.ino
@@ -1,23 +1,9 @@
-// Set true if using common anode, false if using common cathode
-const boolean COMMON_ANODE = false; 
+const boolean IS_COMMON_ANODE = false; 
 
 const int RGB_RED_PIN = 6;
-const int RGB_GREEN_PIN  = 5;
-const int RGB_BLUE_PIN  = 3;
-const int DELAY_MS = 20; // delay in ms between changing colors
-const int MAX_COLOR_VALUE = 255;
-
-enum RGB{
-  RED,
-  GREEN,
-  BLUE,
-  NUM_COLORS
-};
-
-int _rgbLedValues[] = {255, 0, 0}; // Red, Green, Blue
-enum RGB _curFadingUpColor = GREEN;
-enum RGB _curFadingDownColor = RED;
-const int FADE_STEP = 5;  
+const int RGB_GREEN_PIN = 5;
+const int RGB_BLUE_PIN = 3;
+const int DELAY_MS = 1000; // delay in ms between changing colors
 
 void setup() {
   // Set the RGB pins to output
@@ -27,72 +13,68 @@ void setup() {
 
   // Turn on Serial so we can verify expected colors via Serial Monitor
   Serial.begin(9600); 
-  Serial.println("Red, Green, Blue");
-
-  // Set initial color
-  setColor(_rgbLedValues[RED], _rgbLedValues[GREEN], _rgbLedValues[BLUE]);
-  delay(DELAY_MS);
 }
 
 void loop() {
 
-  // Increment and decrement the RGB LED values for the current
-  // fade up color and the current fade down color
-  _rgbLedValues[_curFadingUpColor] += FADE_STEP;
-  _rgbLedValues[_curFadingDownColor] -= FADE_STEP;
-
-  // Check to see if we've reached our maximum color value for fading up
-  // If so, go to the next fade up color (we go from RED to GREEN to BLUE
-  // as specified by the RGB enum)
-  // This fade code partially based on: https://gist.github.com/jamesotron/766994
-  if(_rgbLedValues[_curFadingUpColor] > MAX_COLOR_VALUE){
-    _rgbLedValues[_curFadingUpColor] = MAX_COLOR_VALUE;
-    _curFadingUpColor = (RGB)((int)_curFadingUpColor + 1);
-
-    if(_curFadingUpColor > (int)BLUE){
-      _curFadingUpColor = RED;
-    }
-  }
-
-  // Check to see if the current LED we are fading down has gotten to zero
-  // If so, select the next LED to start fading down (again, we go from RED to 
-  // GREEN to BLUE as specified by the RGB enum)
-  if(_rgbLedValues[_curFadingDownColor] < 0){
-    _rgbLedValues[_curFadingDownColor] = 0;
-    _curFadingDownColor = (RGB)((int)_curFadingDownColor + 1);
-
-    if(_curFadingDownColor > (int)BLUE){
-      _curFadingDownColor = RED;
-    }
-  }
-
-  // Set the color and then delay
-  setColor(_rgbLedValues[RED], _rgbLedValues[GREEN], _rgbLedValues[BLUE]);
+  // red
+  Serial.print("Red: ");
+  setRgbLedColor(HIGH, LOW, LOW);
+  delay(DELAY_MS);
+  
+  // green
+  Serial.print("Green: ");
+  setRgbLedColor(LOW, HIGH, LOW);
+  delay(DELAY_MS);
+  
+  // blue
+  Serial.print("Blue: ");
+  setRgbLedColor(LOW, LOW, HIGH);
+  delay(DELAY_MS);
+  
+  // purple
+  Serial.print("Purple: ");
+  setRgbLedColor(HIGH, LOW, HIGH);
+  delay(DELAY_MS);
+  
+  // turqoise
+  Serial.print("Turqoise: ");
+  setRgbLedColor(LOW, HIGH, HIGH);
+  delay(DELAY_MS);
+  
+  // yellow
+  Serial.print("Yellow: ");
+  setRgbLedColor(HIGH, HIGH, LOW);
+  delay(DELAY_MS);
+  
+  // white
+  Serial.print("White: ");
+  setRgbLedColor(HIGH, HIGH, HIGH);
   delay(DELAY_MS);
 }
 
+
 /**
- * setColor takes in values between 0 - 255 for the amount of red, green, and blue, respectively
- * where 255 is the maximum amount of that color and 0 is none of that color. You can illuminate
- * all colors by intermixing different combinations of red, green, and blue
+ * setRgbLedColor takes in either HIGH or LOW for red, green, and blue.
  * 
  * This function is based on https://gist.github.com/jamesotron/766994
  */
-void setColor(int red, int green, int blue)
+void setRgbLedColor(int red, int green, int blue)
 {
+  // If a common anode LED, invert values
+  if(IS_COMMON_ANODE == true){
+    red = !red;
+    green = !green;
+    blue = !blue;
+  }
+
   Serial.print(red);
   Serial.print(", ");
   Serial.print(green);
   Serial.print(", ");
   Serial.println(blue);
-
-  // If a common anode LED, invert values
-  if(COMMON_ANODE == true){
-    red = MAX_COLOR_VALUE - red;
-    green = MAX_COLOR_VALUE - green;
-    blue = MAX_COLOR_VALUE - blue;
-  }
-  analogWrite(RGB_RED_PIN, red);
-  analogWrite(RGB_GREEN_PIN, green);
-  analogWrite(RGB_BLUE_PIN, blue);  
+  
+  digitalWrite(RGB_RED_PIN, red);
+  digitalWrite(RGB_GREEN_PIN, green);
+  digitalWrite(RGB_BLUE_PIN, blue);  
 }
